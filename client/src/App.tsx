d435da291290f831f8b5bdc6683b930b70d72dbd
Alessandro Salzo <58805919+Tarkbyte@users.noreply.github.com>
2022-07-01 00:01:32 +0200

    Added footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import ExpensesList from "./components/ExpensesList";
 import AddExpenseForm from "./components/AddExpenseForm";
 import Toggle from "./components/Toggle";
+import Footer from "./components/Footer";
 
 const App = () => {
   const currency = localStorage.getItem("currency") === "true";
@@ -16,17 +17,20 @@ const App = () => {
   return (
     <div className="App">
       <div className="flex justify-center items-center h-screen app-bg">
-        <div className="bg-white/30 backdrop-blur-sm p-10 rounded-xl w-full mx-80 border border-white border-opacity-10 shadow flex justify-between">
-          <div className="w-full">
-            <div className="flex justify-between items-center">
-              <p className="font-black uppercase">
-                Your expenses will appear below:
-              </p>
-              <Toggle enabled={enabled} setEnabled={setEnabled} />
+        <div className="bg-white/30 backdrop-blur-sm px-10 pt-10 rounded-xl w-full mx-80 border border-white border-opacity-10 shadow">
+          <div className="flex justify-between border-b-2 border-emerald-400 pb-10">
+            <div className="w-full">
+              <div className="flex justify-between items-center">
+                <p className="font-black uppercase">
+                  Your expenses will appear below:
+                </p>
+                <Toggle enabled={enabled} setEnabled={setEnabled} />
+              </div>
+              <ExpensesList enabled={enabled} />
             </div>
-            <ExpensesList enabled={enabled} />
+            <AddExpenseForm />
           </div>
-          <AddExpenseForm />
+          <Footer />
         </div>
       </div>
     </div>
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,28 @@
-type Props = {};
+import { HeartIcon } from "@heroicons/react/outline";
 
-const Footer = (props: Props) => {
-  return <div>Footer</div>;
+const Footer = () => {
+  return (
+    <div className="w-full p-5">
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="text-xs">
+            Watch the source code on{" "}
+            <a
+              href="https://github.com/Tarkbyte/expenses-app"
+              target="_blank"
+              className="text-emerald-500"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
+        <p className="flex text-xs items-center">
+          Created with <HeartIcon className="w-5 h-5 text-emerald-500 mx-1" />
+          by Alessandro Salzo
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
